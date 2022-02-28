36b30d7878fdcacb3fc656060f95f2683c9535a5
aaronlaib <aaron.laib@cru.org>
2022-02-28 17:35:38 -0600

    created function and ran through tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = (string, num) => {
+  if (num < 0) {
+    return "ERROR";
+  } else if (num >= 0) {
+    let newString = "";
+    for (let i = 0; i < num; i++) {
+      newString += string;
+    }
+    return newString;
+  }
 };
+
+//console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
