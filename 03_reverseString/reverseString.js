fe5cc7d90711a80b41b189ee52419fdcb2259929
aaronlaib <aaron.laib@cru.org>
2022-03-01 09:58:18 -0600

    created function and passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = (string) => {
+    let strArray = string.split('');
 
+    let newArray = [];
+
+    for (let i = strArray.length - 1; i >= 0; i--) {
+        newArray.push(strArray[i]);
+    }
+
+    const newStr = newArray.join("");
+
+    return newStr;
 };
+
+//console.log(reverseString("Hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
