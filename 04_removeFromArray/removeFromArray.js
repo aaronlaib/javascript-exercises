e90e9cd7730229ddad6040445b979d7afd8a9763
aaronlaib <aaron.laib@cru.org>
2022-03-15 16:54:28 -0500

    created function and passsed all jest tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = (array, ...args) => {
+  let newArray = [];
 
+  array.forEach((element) => {
+    if (!args.includes(element)) {
+      newArray.push(element);
+    }
+  });
+
+  return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
