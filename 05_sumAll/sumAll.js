e7ade840fa8b75ab84d213d056b328b7b179ac67
aaronlaib <aaron.laib@cru.org>
2022-03-17 11:19:58 -0500

    able to get function working and tests to pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+  let sum = 0;
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+  else if (a > b) {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  } else if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  }
 
+  return sum;
 };
+
+//console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
