e746e375dc9075735adb272fe66d2815efc3da0b
aaronlaib <aaron.laib@cru.org>
2022-03-17 11:47:01 -0500

    created function and passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = (year) => {
+    
+    if (year % 4 == 0 && !(year % 100 == 0)){
+        return true;
+    }
+    if (year % 400 == 0) {
+        return true;
+    }
+    else if (year % 100 == 0) {
+        return false;
+    }
+    else {
+        return false;
+    }
 
 };
 
